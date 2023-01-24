497b0d6e3e2b6b1ed64e635522661980070ab63d
Barış Karakoç <baris1karakoc@gmail.com>
2023-01-24 15:08:59 +0300

    HashSet exercise

---
+++ src/HashSet1.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class HashSet1 {
+    public static void main(String[] args) {
+
+        HashSet<String> hashSet = new HashSet<String>();
+
+        hashSet.add("Red");
+        hashSet.add("Green");
+        hashSet.add("Black");
+        hashSet.add("White");
+        hashSet.add("Pink");
+        hashSet.add("Yellow");
+
+        System.out.println("Hash Set: " + hashSet);
+    }
+
+}
